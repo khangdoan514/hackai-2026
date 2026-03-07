b289c5225e28778a949a631fe214325225d403d9
Khang Doan <kdoan.personal@gmail.com>
2026-03-07 15:48:52 -0600

    Added Play page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
-import About from "./pages/About.jsx"
 import NavBar from "./components/Navbar.jsx"
+import About from "./pages/About.jsx"
 import Home from "./pages/Home.jsx"
+import Play from "./pages/Play.jsx"
 import { Routes, Route } from "react-router-dom"
 import './App.css'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-black text-3xl text-center">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/about" element={ <About /> } />
-      </Routes>
+    <div className="min-h-screen relative">
+      <div className="absolute inset-0 bg-white z-10"></div>
+      <div className="absolute inset-0 bg-black text-3xl text-center rounded-xl m-5 z-20">
+        <div className="bg-white max-w-4xl mx-auto rounded-b-xl">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/about" element={ <About /> } />
+            <Route path="/play" element={ <Play /> } />
+          </Routes>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,9 @@
 export default function About() {
-    return (
-        <div className="">
-            <h1 className="text-2xl font-bold">
-                This is the about page
-            </h1>
-        </div>
-    )
+  return (
+    <div className="bg-black text-white">
+      <h1 className="text-2xl font-bold">
+        This is the about page
+      </h1>
+    </div>
+  )
 }
